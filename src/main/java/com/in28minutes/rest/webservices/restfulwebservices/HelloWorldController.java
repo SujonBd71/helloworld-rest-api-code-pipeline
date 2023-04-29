2ad9124f5a5f00b9c02a996b06b7ca06b736586b
LAPTOP-CTHNC5P1\ssujo <s.sujon1231@gmail.com>
2023-04-29 02:23:25 -0400

    ip

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
-	public String helloWorld() {
-		return "Hello World CI";
+	public String helloWorld() throws UnknownHostException {
+		InetAddress IP=InetAddress.getLocalHost();
+		System.out.println(IP.toString());
+		String response = "IP" + IP.toString();
+		return response;
 	}
 
 	@GetMapping(path = "/hello-world-bean")
