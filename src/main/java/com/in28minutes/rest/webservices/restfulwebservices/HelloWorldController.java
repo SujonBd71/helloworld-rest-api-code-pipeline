b04c1042194d91b72bfa25c3172f1fb7eb853601
LAPTOP-CTHNC5P1\ssujo <s.sujon1231@gmail.com>
2024-02-17 21:18:36 -0500

    add healthcheck

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -3,10 +3,17 @@ package com.in28minutes.rest.webservices.restfulwebservices;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import javax.swing.Spring;
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.RequestMethod;
 @RestController
 public class HelloWorldController {
 
@@ -22,9 +29,30 @@ public class HelloWorldController {
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World - Changed - V3");
 	}
+
+	
+	@GetMapping(path = "/health")
+	public HelloWorldBean health() {
+		return new HelloWorldBean("health-check success");
+	}
+	
 	
 	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
+
+
+
+
+	@RequestMapping(value = "/greeting", method = RequestMethod.POST , consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public String greetingJson(@RequestBody  String raw) {
+		System.out.println("recieved below request");
+		System.out.println(raw);
+		System.out.println();
+		System.out.println("done");
+		return "OK";
+	}
+
 }
